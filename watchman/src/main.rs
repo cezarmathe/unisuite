bc87971e12c082983d8202d702a2796dcfab24e4
cezarmathe <me@cezarmathe.com>
2021-01-07 10:09:50 +0200

    Add SIGTERM signal listener

--- watchman/src/main.rs
+++ watchman/src/main.rs
@@ -2,9 +2,13 @@ mod watcher;
 
 use actix::prelude::*;
 
+use actix_rt::signal;
+
 use watcher::WatcherActor;
 
 #[actix_rt::main]
 async fn main() {
     WatcherActor::new(vec![]).start();
+
+    signal::ctrl_c().await.unwrap();
 }

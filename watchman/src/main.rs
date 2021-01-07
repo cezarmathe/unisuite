23ba3e52522ca80b27114cf5d44bceca6432c6c0
cezarmathe <me@cezarmathe.com>
2021-01-07 12:35:16 +0200

    watchman: Fix bug that makes watchman not watch

--- watchman/src/main.rs
+++ watchman/src/main.rs
@@ -11,7 +11,7 @@ async fn main() {
     let rules = std::env::var("USSCRAPER_RULES").unwrap();
     let rules_vec: Vec<String> = rules.split(",").map(|s| s.to_string()).collect();
 
-    WatcherActor::new(rules_vec).start();
+    let addr = WatcherActor::new(rules_vec).start();
 
     signal::ctrl_c().await.unwrap();
 }

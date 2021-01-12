c90ef4d3b5561a6537b67495c4253b515274c94f
cezarmathe <me@cezarmathe.com>
2021-01-12 21:57:18 +0200

    watchman: Implement proper logging with uslib

--- watchman/src/main.rs
+++ watchman/src/main.rs
@@ -4,21 +4,39 @@ extern crate anyhow;
 mod config;
 mod watcher;
 
-use actix::prelude::*;
+use watcher::RuleWatcher;
 
-use actix_rt::signal;
+use std::sync::Mutex;
 
-use watcher::WatcherActor;
+use uslib::LOGGER;
 
-#[actix_rt::main]
-async fn main() {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let rules = std::env::var("USSCRAPER_RULES").unwrap();
     let rules_vec: Vec<config::ScraperRule> = rules
         .split(",")
         .map(|s| config::ScraperRule::new(s.to_string()))
         .collect();
 
-    let addr = WatcherActor::new(rules_vec).start();
+    uslib::info!(LOGGER, "hello world\n");
 
-    signal::ctrl_c().await.unwrap();
+    let rule_watcher_mutex = Mutex::new(RuleWatcher::new(rules_vec)?);
+    {
+        let mut rule_watcher = rule_watcher_mutex.lock().unwrap();
+        rule_watcher.start().await?;
+    }
+
+    tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())?
+        .recv()
+        .await
+        .unwrap();
+
+    {
+        let mut rule_watcher = rule_watcher_mutex.lock().unwrap();
+        rule_watcher.stop().await?;
+    }
+
+    uslib::info!(LOGGER, "goodbye\n");
+
+    Ok(())
 }
--- watchman/src/watcher.rs
+++ watchman/src/watcher.rs
@@ -5,9 +5,9 @@ use crate::config::ScraperRule;
 use std::convert::TryFrom;
 use std::path::PathBuf;
 
-use actix::prelude::*;
-
 use hotwatch::{Event, Hotwatch};
+
+use uslib::LOGGER;
 
 /// An event raised by the watcher.
 /// We only really care if the file changed, at this point.
@@ -27,55 +27,79 @@ impl WatcherEvent {
     }
 }
 
-pub struct WatcherActor {
+pub struct RuleWatcher {
     rules: Vec<ScraperRule>,
     hw: Hotwatch,
 }
 
-impl WatcherActor {
-    pub fn new(rules: Vec<ScraperRule>) -> Self {
-        Self {
+impl RuleWatcher {
+    /// Create a new rule watcher.
+    pub fn new(rules: Vec<ScraperRule>) -> anyhow::Result<Self> {
+        Ok(Self {
             rules,
-            hw: Hotwatch::new().expect("hotwatch failed to initialize!"),
-        }
+            hw: Hotwatch::new()?,
+        })
     }
-}
 
-impl Actor for WatcherActor {
-    type Context = Context<Self>;
-
-    fn started(&mut self, ctx: &mut Self::Context) {
-        // create the event handler lambda
-        // this will log the event and send a message to the asbot client actor
-        let event_handler = |event: Event| {
-            let mut rule: Option<ScraperRule> = None;
-            match event {
-                Event::Create(path) => rule = Some(ScraperRule::try_from(path.as_path()).unwrap()),
-                Event::Write(path) => rule = Some(ScraperRule::try_from(path.as_path()).unwrap()),
-                Event::Error(err, path) => {
-                    println!(
-                        "Event::Error, path: {}, err: {}",
-                        path.unwrap_or_default().display(),
-                        err
-                    );
-                }
-                _ => {
-                    println!("event: {:?}", event);
-                }
-            }
-            if let Some(val) = rule {
-                let watcher_event = WatcherEvent::new(val);
-                println!("rule updated: {:?}", watcher_event);
-            }
-        };
-
-        // watch all rules
+    /// Start the rule watcher.
+    ///
+    /// This will start watching all rules.
+    pub async fn start(&mut self) -> anyhow::Result<()> {
         for rule in self.rules.as_slice() {
-            println!("Watching rule {:?}", rule);
+            uslib::info!(LOGGER, "watching rule {:?}\n", rule);
             let path: &PathBuf = &rule.into();
-            self.hw
-                .watch(path, event_handler)
-                .expect("failed to watch file!");
+            self.hw.watch(path, Self::handle_event)?
+        }
+
+        Ok(())
+    }
+
+    /// Stop the rule watcher.
+    ///
+    /// This will stop watching all rules.
+    pub async fn stop(&mut self) -> anyhow::Result<()> {
+        for rule in self.rules.as_slice() {
+            uslib::info!(LOGGER, "unwatching rule {:?}\n", rule);
+            let path: &PathBuf = &rule.into();
+            self.hw.unwatch(path)?
+        }
+        Ok(())
+    }
+
+    fn handle_event(event: Event) {
+        let mut rule: Option<ScraperRule> = None;
+        match event {
+            Event::Create(path) => match ScraperRule::try_from(path.as_path()) {
+                Ok(val) => rule = Some(val),
+                Err(e) => uslib::warn!(
+                    LOGGER,
+                    "rule watcher: failed to get rule from path: {}\n",
+                    e
+                ),
+            },
+            Event::Write(path) => match ScraperRule::try_from(path.as_path()) {
+                Ok(val) => rule = Some(val),
+                Err(e) => uslib::warn!(
+                    LOGGER,
+                    "rule watcher: failed to get rule from path: {}\n",
+                    e
+                ),
+            },
+            Event::Error(err, path) => {
+                uslib::error!(
+                    LOGGER,
+                    "rule watcher: error event received: {}, path {:?}\n",
+                    err,
+                    path
+                );
+            }
+            _ => {
+                uslib::debug!(LOGGER, "rule watcher: received other event: {:?}\n", event);
+            }
+        }
+        if let Some(val) = rule {
+            let watcher_event = WatcherEvent::new(val);
+            uslib::info!(LOGGER, "rule watcher: watcher event: {:?}\n", watcher_event);
         }
     }
 }

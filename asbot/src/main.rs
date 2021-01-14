306321dbaa163bd305b01a9ee33976bbcd397f49
cezarmathe <me@cezarmathe.com>
2021-01-14 09:54:06 +0200

    asbot: Create boilerplate

--- asbot/src/main.rs
+++ asbot/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+//! Adam Smith bot.
+
+#[tokio::main]
+async fn main() {
+    uslib::info!(uslib::LOGGER, "main: hello world\n");
+
+    // initialization
+
+    // start
+
+    // wait for termination
+
+    uslib::debug!(uslib::LOGGER, "main: waiting for termination signal\n");
+    tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+        .unwrap()
+        .recv()
+        .await
+        .unwrap();
+    uslib::debug!(uslib::LOGGER, "main: received termination signal, proceeding with graceful shutdown\n");
+
+    // graceful shutdown
+
+    // done
+
+    uslib::info!(uslib::LOGGER, "goodbye\n");
 }

01061d84b05dc9136c3e234a5bc3e41ff66c6b10
cezarmathe <me@cezarmathe.com>
2021-01-14 22:10:03 +0200

    asbot: Implement basic mevents server

--- asbot/src/server.rs
+++ asbot/src/server.rs
@@ -1,6 +1,16 @@
 //! Adam Smith bot gRPC server.
 
+use std::cell::RefCell;
+
 use uslib::parking_lot::Mutex;
+use uslib::proto::moodle_events_server::MoodleEvents;
+use uslib::proto::moodle_events_server::MoodleEventsServer;
+use uslib::proto::NotifyRequest;
+use uslib::proto::NotifyResponse;
+use uslib::tonic::Request;
+use uslib::tonic::Response;
+use uslib::tonic::Status;
+use uslib::tonic::transport::Server;
 
 /// gRPC server.
 pub static SERVER: uslib::OnceCell<Mutex<GrpcServer>> = uslib::OnceCell::new();
@@ -20,7 +30,10 @@ impl GrpcServerConfig {
 }
 
 #[derive(Debug)]
-pub struct GrpcServer {}
+pub struct GrpcServer {
+    inner: RefCell<Server>,
+    mevents: MoodleEventsService,
+}
 
 impl GrpcServer {
     /// Initialize the AsBotClient.
@@ -28,9 +41,12 @@ impl GrpcServer {
         uslib::trace!(uslib::LOGGER, "grpc server: init\n");
         let config = GrpcServerConfig::load().await?;
 
+        let inner = Server::builder();
+        let mevents = MoodleEventsService;
+
         uslib::trace!(uslib::LOGGER, "grpc server: init: setting up singleton\n");
         if SERVER
-            .set(Mutex::new(GrpcServer {}))
+            .set(Mutex::new(GrpcServer { inner: RefCell::new(inner), mevents }))
             .is_err()
         {
             uslib::bail!("grpc server: init: already initialized\n");
@@ -44,6 +60,14 @@ impl GrpcServer {
     pub async fn start() -> uslib::Result<()> {
         uslib::trace!(uslib::LOGGER, "grpc server: start\n");
         let server = SERVER.get().unwrap().lock();
+
+        let mut inner = server.inner.borrow_mut();
+        let router = inner.add_service(MoodleEventsServer::new(server.mevents.clone()));
+        tokio::spawn(async {
+            uslib::trace!(uslib::LOGGER, "grpc server: start: begin serve\n");
+            router.serve("0.0.0.0:5555".parse().unwrap()).await
+        });
+
         uslib::trace!(uslib::LOGGER, "grpc server: start ok\n");
         Ok(())
     }
@@ -53,5 +77,19 @@ impl GrpcServer {
         let server = SERVER.get().unwrap().lock();
         uslib::trace!(uslib::LOGGER, "grpc server: stop ok\n");
         Ok(())
+    }
+}
+
+#[derive(Clone, Debug)]
+struct MoodleEventsService;
+
+#[uslib::async_trait]
+impl MoodleEvents for MoodleEventsService {
+    async fn notify(
+        &self,
+        request: Request<NotifyRequest>,
+    ) -> Result<Response<NotifyResponse>, Status> {
+        uslib::trace!(uslib::LOGGER, "grpc server: moodle events: notify\n");
+        Ok(Response::new(NotifyResponse {}))
     }
 }

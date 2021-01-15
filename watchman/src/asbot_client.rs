d82dd1f2449fec715e0ae87909351dd294f20eb4
cezarmathe <me@cezarmathe.com>
2021-01-15 06:46:27 +0200

    watchman: Remove useless import

--- watchman/src/asbot_client.rs
+++ watchman/src/asbot_client.rs
@@ -2,7 +2,6 @@
 
 use uslib::parking_lot::Mutex;
 use uslib::proto::moodle_events_client::MoodleEventsClient;
-use uslib::proto::NotifyResponse;
 
 /// Adam Smith bot client.
 pub static CLIENT: uslib::OnceCell<Mutex<AsBotClient>> = uslib::OnceCell::new();

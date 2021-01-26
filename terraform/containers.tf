366ceada8f5363166ef8bd8855e11833746cfbb4
cezarmathe <me@cezarmathe.com>
2021-01-26 11:52:01 +0200

    terraform: Fix env name for asbot address

--- terraform/containers.tf
+++ terraform/containers.tf
@@ -99,7 +99,7 @@ resource "docker_container" "asbot" {
   env = [
     "SYSLOG=${docker_container.syslog.network_data[0].ip_address}:514",
     "LOG_LEVEL=${var.asbot_log_level != "" ? var.asbot_log_level : var.log_level}",
-    "ASBOT_GRPC_PORT=${random_integer.asbot_grpc_port.result}",
+    "ASBOT_ADDRESS=http://0.0.0.0:${random_integer.asbot_grpc_port.result}",
     "ASBOT_DISCORD_TOKEN=${var.asbot_discord_token}",
     "ASBOT_DISCORD_WEBHOOK_ID=${var.asbot_mevents_webhook_id}",
     "ASBOT_DISCORD_WEBHOOK_TOKEN=${var.asbot_mevents_webhook_token}",

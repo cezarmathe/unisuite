6e44abb5ad08a3c399e84619bada197d88a17f1c
cezarmathe <me@cezarmathe.com>
2021-02-01 08:59:16 +0200

    usdiff: Add termination signal waiting

--- usdiff/src/main.rs
+++ usdiff/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+//! Unisuite diff(er).
+
+use uslib::common::*;
+
+#[tokio::main]
+async fn main() {
+    slog::info!(uslib::LOGGER, "main: hello world\n");
+
+    slog::debug!(uslib::LOGGER, "main: waiting for termination signal\n");
+    tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+        .unwrap()
+        .recv()
+        .await
+        .unwrap();
+    slog::debug!(
+        uslib::LOGGER,
+        "main: received termination signal, proceeding with graceful shutdown\n"
+    );
+
+    slog::info!(uslib::LOGGER, "goodbye\n");
 }
